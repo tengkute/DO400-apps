2495fff43788932636126d16fb778ffb9a71fdc3
tengkute <TengkuNurnadia.TengkuMajani@dhl.com>
2022-08-05 02:46:41 -0400

    Refactor

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const url = require("url");
-const greet = require("./greet");
+const greeting = greet(name);
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greet(name));
+    res.end(greeting);
 });
 
 const port = 3000;
